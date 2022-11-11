9987b4f172e57d7fd3e1df27193e418ab68bee62
Georgiy Kamnev <nt.gocha@gmail.com>
2022-11-12 00:30:29 +0500

    state model

--- src/main/scala/xyz/cofe/jtfm/store/json/stream/number.scala
+++ src/main/scala/xyz/cofe/jtfm/store/json/stream/number.scala
@@ -55,71 +55,106 @@ package xyz.cofe.jtfm.store.json.stream
  */
 object number:
   /** Модель состояний автомата */
-  enum State:
+  enum State extends StreamTokenParserState:
     // '-' -> DecStart
     // '0' -> DecPref
     // '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> DecPart
     // '.' -> FloatExpectFraction
     // -> Err
-    case Init
+    case Init extends State
 
     // '0' -> DecPref
     // '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> DecPart
     // '.' ->
-    case DecStart( positive:Boolean=true )
+    case DecStart( positive:Boolean=true ) extends State
 
     // 'x' | 'X' -> HexInt
     // 'b' | 'B' -> BinInt
     // 'o' | 'O' -> OctInt
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' -> OctInt
     // -> Finish
-    case DecPref( positive:Boolean=true )
+    case DecPref( positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> DecPart
     // 'n' -> FinishConsumed
     // '.' -> FloatAfterPoint
-    case DecPart( digit:List[Int]=List(), positive:Boolean=true )
+    case DecPart( digit:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
     //     | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' 
     //     | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' -> HexInt
     // 'n' -> FinishConsumed
     // -> Finish
-    case HexInt( digit:List[Int]=List(), positive:Boolean=true )
+    case HexInt( digit:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' -> BinInt
     // 'n' -> FinishConsumed
     // -> Finish
-    case BinInt( digit:List[Int]=List(), positive:Boolean=true )
+    case BinInt( digit:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' -> OctInt
     // 'n' -> FinishConsumed
     // -> Finish
-    case OctInt( digit:List[Int]=List(), positive:Boolean=true )
+    case OctInt( digit:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> FloatFraction
     // 'e' | 'E' -> ExpoSignOpt
     // -> Finish
-    case FloatAfterPoint( dec:List[Int]=List(), positive:Boolean=true )
+    case FloatAfterPoint( dec:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> FloatFraction
     // 'e' | 'E' -> ExpoSignOpt
     // -> Finish
-    case FloatFraction( dec:List[Int]=List(), fraction:List[Int]=List(), positive:Boolean=true )
+    case FloatFraction( dec:List[Int]=List(), fraction:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> FloatFraction
     // -> Err
-    case FloatExpectFraction( dec:List[Int]=List(), positive:Boolean=true )
+    case FloatExpectFraction( dec:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '+' | '-' -> Expo
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' ->  Expo
     // -> Err
-    case ExpoSignOpt( dec:List[Int]=List(), fraction:List[Int]=List(), positive:Boolean=true )
+    case ExpoSignOpt( dec:List[Int]=List(), fraction:List[Int]=List(), positive:Boolean=true ) extends State
 
     // '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' ->  Expo
     // -> Finish
-    case Expo( dec:List[Int]=List(), fraction:List[Int]=List(), expoPositive:Boolean=true, positive:Boolean=true )
+    case Expo( dec:List[Int]=List(), fraction:List[Int]=List(), expoPositive:Boolean=true, positive:Boolean=true ) extends State
 
-    case Finish( base:Int, float:Boolean, big:Boolean, dec:List[Int]=List(), fraction:List[Int]=List(), expo:List[Int]=List(), expoPositive:Boolean=true, positive:Boolean=true )
-    case FinishConsumed( base:Int, float:Boolean, big:Boolean, dec:List[Int]=List(), fraction:List[Int]=List(), expo:List[Int]=List(), expoPositive:Boolean=true, positive:Boolean=true )
-    case Err
+    case Finish( 
+      base:Int, 
+      float:Boolean,
+      big:Boolean, 
+      dec:List[Int]=List(), 
+      fraction:List[Int]=List(), 
+      expo:List[Int]=List(), 
+      expoPositive:Boolean=true, 
+      positive:Boolean=true 
+    ) extends State
+    case FinishConsumed( 
+      base:Int, 
+      float:Boolean, 
+      big:Boolean, 
+      dec:List[Int]=List(), 
+      fraction:List[Int]=List(), 
+      expo:List[Int]=List(), 
+      expoPositive:Boolean=true, 
+      positive:Boolean=true 
+    ) extends State
+    case Err extends State
+
+    override def isAcceptable: Boolean = this match
+      case State.Err => false
+      case _:State.Finish => false
+      case _ => true
+    
+    override def isReady: Boolean = this match
+      case _:State.Finish => true
+      case _:State.FinishConsumed => true
+      case _ => false
+    
+    override def isError: Boolean = this match
+      case State.Err => true
+      case _ => false
+    override def isConsumed: Boolean = this match
+      case _:State.FinishConsumed => true
+      case _ => false
