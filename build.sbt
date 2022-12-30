fce58b854cbae472c094aaa86cd8edea0eddddb3
Georgiy Kamnev <nt.gocha@gmail.com>
2022-12-31 00:42:54 +0500

    v 0.1.2

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ lazy val root = project
   .in(file("."))
   .settings(
     name := "json4s3",
-    version := "0.1.1",
+    version := "0.1.2",
 
     scalaVersion := scala3Version,
 
--- src/main/scala/xyz/cofe/json4s3/derv/package.scala
+++ src/main/scala/xyz/cofe/json4s3/derv/package.scala
@@ -4,6 +4,7 @@ import derv.errors._
 import xyz.cofe.json4s3.stream.token.Tokenizer
 import xyz.cofe.json4s3.stream.ast.Parser
 import xyz.cofe.json4s3.stream.ast.AST
+import xyz.cofe.json4s3.stream.ast.FormattingJson
 
 package object derv {
   extension (string:String)
@@ -14,7 +15,7 @@ package object derv {
         }
       }
 
-  extension [A:ToJson]( item:A )
+  extension [A:ToJson]( item:A )(using formatting:FormattingJson)
     def asJson:Option[AST] = 
       summon[ToJson[A]].toJson(item)
     def json:String =
